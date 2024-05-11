e5c4b28f4a74c93d3c425781af5609c302375844
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-05-11 18:06:50 -0300

    #3 - add seller models and jpa mapping

---
+++ sellers-backend/sellers/src/main/java/com/abutua/sellers/models/Seller.java
@@ -0,0 +1,69 @@
+package com.abutua.sellers.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "SELLERS")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private double salary;
+    private double bonus;
+    private int gender;
+
+    public Seller(long id, String name, double salary, double bonus, int gender) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.bonus = bonus;
+        this.gender = gender;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+}
