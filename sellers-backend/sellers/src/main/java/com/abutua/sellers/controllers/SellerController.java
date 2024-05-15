f02e996ea0af9a4b8019cb31fa0be02fda592f57
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-05-15 18:56:20 -0300

    fix, add cross origin

--- sellers-backend/sellers/src/main/java/com/abutua/sellers/controllers/SellerController.java
+++ sellers-backend/sellers/src/main/java/com/abutua/sellers/controllers/SellerController.java
@@ -13,12 +13,14 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.abutua.sellers.models.Seller;
 import com.abutua.sellers.services.SellerService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
+@CrossOrigin
 public class SellerController {
 
     @Autowired
